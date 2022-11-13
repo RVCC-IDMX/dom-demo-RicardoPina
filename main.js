2d5dc792ad0bcdf1778c5865bac987f423473c0e
Ricardo Pina <pinazelaya@gmail.com>
2022-11-13 18:13:21 -0500

    Add form validation

--- main.js
+++ main.js
@@ -6,3 +6,74 @@ https://youtu.be/hdI2bqOjy3c?t=4228
 
 Start at 1:10:29
  */
+
+// // Single element
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('.container'));
+
+// // Multiple element
+// console.log(document.querySelectorAll('item'));
+// console.log(document.getElementsByClassName('item'));
+
+// console.log(document.getElementsByTagName('li'));
+
+// // looping
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+// // Unordered list
+
+// const ul = document.querySelector('.items');
+
+// ul.remove(); // removes all
+
+// ul.lastElementChild.remove(); //removes last
+// ul.firstElementChild.textContent = 'Hello'; // changes text in html
+
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>'; // adds html dynamically
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red'; // change css dynamically
+
+// btn.addEventListener('click', (e) => {
+// e.preventDefault(); // prevents default event behaviour.
+// console.log(e.target);
+// document.querySelector('#my-form').style.background = '#ccc';
+// document.querySelector('body').classList.add('bg-dark'); // adding a css class to an html
+
+// document.querySelector('.items').lastElementChild.innerHTML =
+// '<h1>Hello</h1>';
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter fields'); //clonky
+    msg.classList.add('error'); // add css to msg class
+    msg.innerHTML = 'Please enter all fields';
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement('li');
+
+    li.appendChild(
+      document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
+    );
+
+    userList.appendChild(li);
+
+    //clear the fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
